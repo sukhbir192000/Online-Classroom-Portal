268cbb51cc01f49882c957d2025800ef8ef974f9
Puneet Singh <puneetkalsi99@gmail.com>
2020-09-30 00:08:15 +0530

    update announcement dynamic

--- routes/content.js
+++ routes/content.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const Router=express.Router();
+const passport=require('../config/passport-google-oauth2-strategy');
 const contentController=require('../controllers/content');
 Router.get('/announcements',contentController.announcement);
+//change to isAdmin
+Router.post('/announcements/create',passport.checkAuthentication,contentController.announcementCreate);
 Router.get('/studymaterial',contentController.studyMaterial);
 Router.get('/recordedlectures',contentController.recordedLectures);
 Router.get('/assignments',contentController.assignments);
