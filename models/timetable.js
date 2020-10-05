f1799a23e5918001071f97dbbf61250ecdd92532
Sukhbir Singh <sukhbir.singh0619@gmail.com>
2020-10-05 22:56:42 +0530

    Add timetable schema

---
+++ models/timetable.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const classSubSchema = require('./class-sub');
+
+const timetableSchema = new mongoose.Schema({
+    date:{
+        type: Date
+    },
+    duration:{
+        type: Number
+    },
+    classSub: classSubSchema,
+    teacher: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+const Timetable = mongoose.model('Timetable', timetableSchema);
+module.exports = Timetable;
