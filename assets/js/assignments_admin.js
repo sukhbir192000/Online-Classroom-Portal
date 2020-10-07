eb160796ed432eb579764c6c35eb754feed9564b
Mehak <mehakg99@gmail.com>
2020-10-07 12:48:58 +0530

    Add assgn admin

--- assets/js/assignments_admin.js
+++ assets/js/assignments_admin.js
@@ -1,4 +1,123 @@
-document.querySelector(".add").addEventListener('click',function(e){
+let current_box=null,arr=[], links=[],deleted_files=[];
+let boxelements = document.querySelectorAll('.box');
+for(let i=0;i<boxelements.length;i++){
+    var editbutton = boxelements[i].children[1].children[3].children[0].children[0];
+    editbutton.addEventListener('click',function(e){
+        if(current_box!=null){
+            var editbutton = current_box.children[1].children[3].children[0].children[0];
+            var deletebutton = current_box.children[1].children[3].children[0].children[1];
+            var savebutton =current_box.children[1].children[3].children[1].children[0];
+            var title = current_box.children[0].children[1];
+            var description = current_box.children[1].children[0].children[4];
+            title.contentEditable="false";
+            description.contentEditable="false";
+            title.classList.remove("heading_edit");
+            description.classList.remove("content_edit")
+            deletebutton.classList.remove("hide");
+            editbutton.classList.remove("hide");
+            savebutton.classList.remove("show");
+            title.textContent=arr[0];
+            description.textContent=arr[1];
+            current_box.children[1].children[2].style.pointerEvents = "auto"
+            fileDeleteButtons = current_box.children[1].children[1];
+            current_box.children[1].children[2].classList.remove("show_delete");
+            for(let j=0;j<fileDeleteButtons.childElementCount;j++){
+                fileDeleteButtons.children[j].setAttribute("download","download");
+                fileDeleteButtons.children[j].href = links[j];
+                fileDeleteButtons.children[j].children[2].classList.remove("show_delete");
+                fileDeleteButtons.children[j].style.opacity = 1;
+                fileDeleteButtons.children[j].style.pointerEvents = "auto";
+            }
+            current_box.children[1].children[2].children[0].innerHTML = "";
+            current_box=null;
+            arr=[];
+            links=[];
+            deleted_files = [];
+        }
+        var editbutton = boxelements[i].children[1].children[3].children[0].children[0];
+        var deletebutton = boxelements[i].children[1].children[3].children[0].children[1];
+        var savebutton = boxelements[i].children[1].children[3].children[1].children[0];
+        var title = boxelements[i].children[0].children[1];
+        var description = boxelements[i].children[1].children[0].children[4];
+        arr.push(title.textContent);
+        arr.push(description.textContent);
+        title.contentEditable="true";
+        description.contentEditable="true";
+        title.classList.add("heading_edit");
+        title.setAttribute("title-text","Enter title");
+        description.classList.add("content_edit");
+        description.setAttribute("description-text", "Enter description");
+        deletebutton.classList.add("hide");
+        editbutton.classList.add("hide");
+        savebutton.classList.add("show");
+        current_box=boxelements[i];
+        current_box.children[1].children[2].style.pointerEvents = "none"
+        fileDeleteButtons = current_box.children[1].children[1];
+        for(let j=0;j<fileDeleteButtons.childElementCount;j++){
+            fileDeleteButtons.children[j].removeAttribute("download");
+            links.push(fileDeleteButtons.children[j].href);
+            fileDeleteButtons.children[j].href= "#/";
+            fileDeleteButtons.children[j].children[2].style.cursor = "pointer";
+            fileDeleteButtons.children[j].children[2].classList.add("show_delete");
+            current_box.children[1].children[2].classList.add("show_delete");
+            fileDeleteButtons.children[j].children[2].addEventListener("click",function(e){
+                deleted_files.push(links[j]);
+                fileDeleteButtons.children[j].children[2].classList.remove("show_delete");
+                fileDeleteButtons.children[j].style.pointerEvents = "none";
+                fileDeleteButtons.children[j].style.opacity = 0.5;
+            });
+        }
+        savebutton.addEventListener('click',function(e){
+            current_box.children[1].children[3].children[1].children[1].children[0].setAttribute('value',title.textContent);
+            current_box.children[1].children[3].children[1].children[1].children[1].setAttribute('value',description.textContent);
+            current_box.children[1].children[3].children[1].children[1].children[2].setAttribute('value',deleted_files);
+            current_box.children[1].children[3].children[1].children[1].submit();
+        })
+    })   
+}
+
+
+
+
+document.addEventListener('click',function(e){
+    if(current_box){
+        var rect = current_box.getBoundingClientRect();
+        var savebutton =current_box.children[1].children[3].children[1].children[0];
+        const style = getComputedStyle(savebutton);
+        if((e.x<rect.left || e.x>rect.left+rect.width || e.y<rect.top || e.y>rect.top+rect.height) && style.visibility=='visible'){
+            var editbutton = current_box.children[1].children[3].children[0].children[0];
+            var deletebutton = current_box.children[1].children[3].children[0].children[1];
+            var title = current_box.children[0].children[1];
+            var description = current_box.children[1].children[0].children[4];
+            title.contentEditable="false";
+            description.contentEditable="false";
+            title.classList.remove("heading_edit");
+            description.classList.remove("content_edit")
+            deletebutton.classList.remove("hide");
+            editbutton.classList.remove("hide");
+            savebutton.classList.remove("show");
+            current_box.children[1].children[2].style.pointerEvents = "auto"
+            fileDeleteButtons = current_box.children[1].children[1];
+            current_box.children[1].children[2].classList.remove("show_delete");
+            for(let j=0;j<fileDeleteButtons.childElementCount;j++){
+                fileDeleteButtons.children[j].setAttribute("download","download");
+                fileDeleteButtons.children[j].href = links[j];
+                fileDeleteButtons.children[j].children[2].classList.remove("show_delete");
+                fileDeleteButtons.children[j].style.opacity = 1;
+                fileDeleteButtons.children[j].style.pointerEvents = "auto";
+            }
+            title.textContent=arr[0];
+            description.textContent=arr[1];
+            subject.textContent=arr[2];
+            current_box=null;
+            arr=[];
+            links=[];
+            deleted_files = [];
+        }
+    }
+})
+
+ document.querySelector(".add").addEventListener('click',function(e){
     if(document.querySelector(".add_content").textContent == "Cancel"){
         document.querySelector(".add_content").textContent = "Add";
         document.querySelector(".add_icon").innerHTML =  "<i class='fas fa-plus'></i>";
