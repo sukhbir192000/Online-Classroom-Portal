3fa2d8a9f10fd70635a45c66794360e0cfd2ee96
Mehak <mehakg99@gmail.com>
2020-10-15 01:20:30 +0530

    Update doubts page

--- assets/js/doubts.js
+++ assets/js/doubts.js
@@ -1,3 +1,46 @@
+(function(){
+    var boxelements = document.querySelectorAll('.box');
+    for(let i=0;i<boxelements.length;i++){
+        materialClass = boxelements[i].children[1].children[1];
+        for(let j=0;j<materialClass.childElementCount;j++){
+            // console.log(materialClass.children[j].href);
+            let extension = materialClass.children[j].href.split('.').pop().toLowerCase();
+            if( extension == 'pptx' || extension == 'ppt' || extension == 'pptm'){
+                materialClass.children[j].children[0].innerHTML = "<i class='fas fa-file-powerpoint'></i>";
+                materialClass.children[j].children[0].classList.add("color_ppt");
+            }
+            else if(extension == 'docx' || extension == 'doc' || extension == 'docm'){
+                materialClass.children[j].children[0].innerHTML = "<i class='fas fa-file-word'></i>";
+                materialClass.children[j].children[0].classList.add("color_word");
+            }
+            else if(extension == 'pdf'){
+                materialClass.children[j].children[0].innerHTML = "<i class='fas fa-file-pdf'></i>";
+                materialClass.children[j].children[0].classList.add("color_pdf");
+            }
+            else if(extension == 'jpg' || extension == 'png' || extension == 'jpeg' || extension == 'gif' || extension == 'tiff'|| extension == 'psd'){
+                materialClass.children[j].children[0].innerHTML = "<i class='fas fa-file-image'></i>";
+                materialClass.children[j].children[0].classList.add("color_image");
+            }
+            else if(extension == 'xlsx' || extension == 'xlsm' || extension == 'xlsb' || extension == 'xls' || extension == 'xml'){
+                materialClass.children[j].children[0].innerHTML = "<i class='fas fa-file-excel'></i>";
+                materialClass.children[j].children[0].classList.add("color_excel"); 
+            }
+            else if(extension == 'mp4' || extension == 'mpeg-4' || extension == 'mov' || extension == 'wmv' || extension == 'flv' || extension == 'avi'){
+                materialClass.children[j].children[0].innerHTML = "<i class='fas fa-file-video'></i>";
+                materialClass.children[j].children[0].classList.add("color_video"); 
+            }
+            else if(extension == 'pcm' || extension == 'wav' || extension == 'mp3' || extension == 'wma' ){
+                materialClass.children[j].children[0].innerHTML = "<i class='fas fa-file-audio'></i>";
+                materialClass.children[j].children[0].classList.add("color_audio"); 
+            }
+            else{
+                materialClass.children[j].children[0].innerHTML = "<i class='fas fa-file-alt'></i>";
+                materialClass.children[j].children[0].classList.add("color_file");
+            }
+        }
+    }
+})();
+
 document.addEventListener('mouseup',function(e){
     var position=document.querySelector(".profile_icon").getBoundingClientRect();
     var rect=document.querySelector(".profile").getBoundingClientRect();
