1f10e6841c31900c1dccc13b8bac38f29cb7ef57
puneetkalsi99 <puneetkalsi99@gmail.com>
2020-11-21 16:01:40 +0530

    add courses assigned controller and route

--- routes/superuser/coursesassigned.js
+++ routes/superuser/coursesassigned.js
@@ -1,5 +1,5 @@
 const coursesController=require('../../controllers/superuser_courses_assigned_controller');
 const express=require('express');
 const Router=express.Router();
-Router.get('/',coursesController.courses);
+Router.get('/',coursesController.courses_assgn);
 module.exports = Router;
